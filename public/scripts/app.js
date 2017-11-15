70a7553b25df02ca8c2e42b466f899227d689b7c
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-15 04:13:37 +0000

    empty out the archive-tweet container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,84 @@
+// Fake data taken from tweets.json
+const allTweets = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+function createTweetElement(tweet) {
+  return $(
+    `<article class="tweet">
+      <header>
+        <div>
+        <img src="${tweet.user.avatars.small}">
+        <h2 class="userName">${tweet.user.name}</h2>
+        </div>
+        <span class="userId">${tweet.user.handle}</span>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>${tweet.created_at}</footer>
+    </article>`
+  );
+};
+
+function renderTweets(data) {
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  var $section = $('#archive-tweet');
+  data.forEach(element => {
+    // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    $section.append(createTweetElement(element)); 
+  });
+};
+
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(function() {
+  renderTweets(allTweets);
+});
