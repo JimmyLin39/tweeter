ba7f22b498efbe8928173923c8b15a81d5e5a2c2
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-16 22:31:58 +0000

    use mongo in saveTweet

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,17 +9,15 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection('tweets').insert(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      db.collection("tweets").find().toArray(callback);
-      db.close();
+      db.collection('tweets').find().toArray(callback);
+      
     }
-
   };
+  db.close();
 }
