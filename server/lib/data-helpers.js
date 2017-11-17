6c0b0b87a5c67e0c38b8b697455ef09b9f7f2cfb
JimmyLin39 <jimmylin0309@gmail.com>
2017-11-17 00:00:48 +0000

    pass callback function while saveTweet in db and remove db.close()

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,15 +9,12 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      db.collection('tweets').insert(newTweet);
-      callback(null, true);
+      db.collection('tweets').insertOne(newTweet, callback);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       db.collection('tweets').find().toArray(callback);
-      
     }
   };
-  db.close();
 }
